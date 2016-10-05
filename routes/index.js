f49abed07484540d78076c198b0c97a1448fe5fe
Sumeet Rohatgi <sum.rohatgi@gmail.com>
2016-10-04 21:50:55 -0700

    added more information

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  const subject = 'algebra-1';
+  res.render('index', { 
+      app_title: 'Tubo',
+      title: `Notes for ${subject}`,
+      cheatsheet: subject
+  });
 });
 
 module.exports = router;
